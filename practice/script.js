8a1f7ebc8e49f5be38c35c51f2f98af8a1951416
gautamkamboj <gautamkamboj0@gmail.com>
2024-01-28 21:08:09 +0530

    geeksterPok

--- practice/script.js
+++ practice/script.js
@@ -1,25 +1,19 @@
-//task1
-//task2
-//task3
-
-
 var promise1 = new Promise(function(res,rej){
-    return res("task1 resolved");
-
+    return res("task1");
 });
 
 var promise2 = promise1.then(function(data){
     console.log(data);
     return new Promise(function(res,rej){
-        return res("task2 resolved");
-    })
+        return res("task2");
+    });
 });
 
 var promise3 = promise2.then(function(data){
     console.log(data);
     return new Promise(function(res,rej){
-        return res("task 3 resolved");
-    })
+        return res("task3");
+    });
 });
 
 promise3.then(function(data){
